8cee2dcdd129c01f4ce6774f70ba22241bb14b9d
Robert Rhys Thomas <gusroberth@GU.GU.SE@eduserv.flov.gu.se>
2019-02-11 14:14:34 +0100

    First commit

---
+++ dialogue_systems/lab_1/vxml-server/app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, make_response, Response
+app = Flask(__name__)
+
+temp = '13'
+
+@app.route('/weather')
+def hello():
+    vxml = render_template('weather.xml', temp=temp)
+    response = make_response(vxml)
+    response.headers["Content-Type"] = "application/xml"
+    return response
+
+@app.route('/lab1')
+def lab1():
+    vxml = render_template('lab1.xml')
+    response = make_response(vxml)
+    response.headers["Content-Type"] = "application/xml"
+    return response
+
+@app.route("/metal.wav")
+def streamwav():
+    def generate():
+        with open("templates/metal.wav", "rb") as fwav:
+            data = fwav.read(1024)
+            while data:
+                yield data
+                data = fwav.read(1024)
+    return Response(generate(), mimetype="audio/x-wav")
+
+
+@app.route("/ogg")
+def streamogg():
+    def generate():
+        with open("signals/song.ogg", "rb") as fogg:
+            data = fogg.read(1024)
+            while data:
+                yield data
+                data = fogg.read(1024)
+    return Response(generate(), mimetype="audio/ogg")
